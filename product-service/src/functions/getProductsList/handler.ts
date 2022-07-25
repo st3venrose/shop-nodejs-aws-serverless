213cc4ac843b2dce0b26c2206b761ce80cf920d0
Istvan Rozsa <rozsa.istvan0701@gmail.com>
2022-07-25 18:44:58 +0200

    feat: add error handling to getAllProduct

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,16 +1,23 @@
-// import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { formatJSONResponse, formatErrorResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { winstonLogger } from "@utils/winstonLogger";
 import { ProductService } from '@services/productService'
 
 const getProductsList = async (event) => {
-  winstonLogger.logRequest(event);
+  try {
+    winstonLogger.logRequest(event);
 
-  const productService = new ProductService();
-  const products = await productService.getAllProducts()
+    const productService = new ProductService();
+    const products = await productService.getAllProducts()
+  
+    return formatJSONResponse(products);
+  } catch (err) {
+    const { NOT_FOUND, INTERNAL_SERVER_ERROR } = StatusCodes
+    winstonLogger.logError(err);
+    return formatErrorResponse(ReasonPhrases.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR);
+  }
 
-  return formatJSONResponse(products);
 };
 
 export const main = middyfy(getProductsList);
